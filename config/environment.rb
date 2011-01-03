e46f7aaec986435eeff4db0c9f1210b4cbf8fe7b
dusan <dusan@dusan-laptop.(none)>
2011-01-03 21:52:46 +0100

    callendar added

--- config/environment.rb
+++ config/environment.rb
@@ -39,3 +39,15 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :domain => "gmail.com",
+   :authentication => :plain,
+   :user_name => "info.aliga",
+   :password => "yebohlav",
+   :enable_starttls_auto => true
+}
+
