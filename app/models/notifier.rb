ab27942ac3dff64d26d2857258500a383005b486
dusan <dusan@dusan-laptop.(none)>
2011-04-30 14:49:53 +0200

    mail fixes

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,16 +1,16 @@
 class Notifier < ActionMailer::Base
 #require 'net/smtp'
   def feedback(user)
-    from "dusaan@gmail.com"
+    from "LetnaLiga.sk"
     recipients "#{user}"
-    subject "Vitajte na portáli aLiga.sk!"
+    subject "Vitajte na LetnaLiga.sk!"
     body :user => user
     content_type 'text/html'
   end
   def invitation(user)
-    from "dusaan@gmail.com"
+    from "LetnaLiga.sk"
     recipients "#{user}"
-    subject "Pozvánka na portál aLiga.sk!"
+    subject "Pozvánka na portál LetnaLiga.sk!"
     body :user => user
     content_type 'text/html'
   end
